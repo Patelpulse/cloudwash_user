bdf2ff3a06815cad7e04dd264c314ffc64d94504
amitsingh790634 <amitsingh790634@gmail.com>
2026-03-29 15:32:08 +0530

    Shorten Vercel build commands

--- cloud_admin/build_vercel.sh
+++ cloud_admin/build_vercel.sh
@@ -3,6 +3,13 @@
 # Exit on error
 set -e
 
+if [ ! -d "flutter" ]; then
+  git clone https://github.com/flutter/flutter.git -b stable --depth 1
+fi
+
+export PATH="$PATH:$PWD/flutter/bin"
+flutter doctor -v
+
 # Create a dummy .env if it doesn't exist (to satisfy dotenv.load())
 if [ ! -f .env ]; then
   echo "Creating dummy .env file"
@@ -13,7 +20,7 @@ fi
 flutter pub get
 
 # Build Flutter Web
-flutter build web --release --dart-define=API_URL=$API_URL
+flutter build web --release --dart-define=API_URL="${API_URL:-https://cloudwashapi.onrender.com/api}"
 
 # Copy build files to the root (Vercel expects them in the directory its told, but we can also just point Vercel to build/web)
 # For now, we'll just keep them in build/web and point Vercel there.
---
+++ cloud_user/build_vercel.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e
+
+if [ ! -d "flutter" ]; then
+  git clone https://github.com/flutter/flutter.git -b stable --depth 1
+fi
+
+export PATH="$PATH:$PWD/flutter/bin"
+flutter doctor -v
+flutter pub get
+flutter build web --release
